a1ab07d0d0ea3c15630b30017724e5037f7f1523
sulthan441 <mohammedsulthan441@gmail.com>
2023-02-26 20:38:10 +0530

    80%

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import Header from './Header'
-function Home() {
-  return (
-    <div>
-        <Header/>
-    </div>
-  )
+import Code from './Code';
+import Result from './Result';
+import Header from './Header';
+
+const Home = () => {
+    return (
+        <>
+            <Header />
+            <Code />
+            {/* <Result /> */}
+        </>
+    )
 }
 
-export default Home
+export default Home;
